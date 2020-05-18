9dd042616f6558f9563d414a8459d730c213279b
Andrew Bykov <42899010+AndZ5556@users.noreply.github.com>
2020-05-19 00:04:58 +0300

    Add files via upload

---
+++ Bykov/lab5/Source/AhoCorasik/main.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+class Bohr
+{
+private:
+	string alphabet = "ACGTN";
+	struct BohrPoint
+	{
+		int directLinks[5]; // переходы в боре
+		bool terminal; // является ли вершина конечной
+		int num; // номер паттерна
+		int parentIndex; // индекс вершины родителя
+		int suffixLink; // суффиксная ссылка
+		int charrLinks[5]; // переходы в автомате
+		int charFromParent; // символ перехода от родителя
+		int compressedLink; // переход по конечной ссылке
+	};
+
+	struct Adjacent
+	{
+		int size;  // размер паттерна
+		int index; // индекс в тексте
+		int num; // номер паттерна
+	};
+
+	vector <Adjacent> pats; // вектор для хранения найденных паттернов
+	string text; // строка поиска
+	vector<BohrPoint> bohr;  // бор
+public:
+	Bohr() // создание бора
+	{
+		cout << "Creating bohr\n";
+		bohr.push_back({ {-1, -1, -1, -1, -1}, false, 0, 0, -1, {-1, -1, -1, -1, -1}, -1, -1 }); // добавляем корневую вершину
+		cin >> text;
+		int c;
+		cin >> c;
+		for (int i = 0; i < c; i++)
+		{
+			string curr;
+			cin >> curr;
+			cout << "Add symbols of new pattern in prefix tree\n";
+			pushPoint(curr, i + 1); // добавляем паттерн в бор
+		}
+	}
+
+	void pushPoint(string str, int number) // функция для добавления паттерна
+	{
+		int index = 0;
+		int charr;
+		for (int i(0); i < str.size(); i++)
+		{
+			switch (str.at(i)) // определяем символ перехода
+			{
+			case 'A':
+				charr = 0;
+				cout << "\nReceived character A" << endl;
+				break;
+			case 'C':
+				charr = 1;
+				cout << "\nReceived character C" << endl;
+				break;
+			case 'G':
+				charr = 2;
+				cout << "\nReceived character G" << endl;
+				break;
+			case 'T':
+				charr = 3;
+				cout << "\nReceived character T" << endl;
+				break;
+			case 'N':
+				charr = 4;
+				cout << "\nReceived character N" << endl;
+				break;
+			}
+			if (bohr[index].directLinks[charr] == -1) // если такого состояния в боре нет, то добавляем новое
+			{
+				cout << "Such way not found, adding new point with number " << bohr.size() << endl;
+				bool isEnd = false;
+				if (i == str.size() - 1)
+					isEnd = true;
+				bohr.push_back({ {-1, -1, -1, -1, -1}, isEnd, number, index, -1, {-1, -1, -1, -1, -1}, charr, -1 });
+				bohr[index].directLinks[charr] = bohr.size() - 1;
+			}
+			index = bohr[index].directLinks[charr];
+			cout << "Switching to point - " << index << "\n";
+			if (i == str.size() - 1) // проверка терминальности вершины
+			{
+				bohr[index].terminal = true;
+				bohr[index].num = number;
+			}
+
+		}
+	}
+
+	int getSuffixLink(int point) // функция для получения суффиксной ссылки
+	{
+		if (bohr.at(point).suffixLink == -1) // если суффиксная ссылка не существует
+		{
+			if (point == 0 || bohr.at(point).parentIndex == 0) // если текущая вершина - корень или если родительская вершина - корень
+				bohr.at(point).suffixLink = 0; // возвращаем 0
+			else // иначе ищем ссылку на нижнем уровне
+				bohr.at(point).suffixLink = getLink(getSuffixLink(bohr.at(point).parentIndex), bohr.at(point).charFromParent);
+		}
+		if (bohr.at(point).suffixLink)
+			cout << "Suffix link to point " << bohr.at(point).suffixLink << "!\n";
+		else
+			cout << "Suffix link not found\n";
+		return bohr.at(point).suffixLink;
+	}
+
+	int getLink(int point, int charr) // функция для получения обычной ссылки
+	{
+		if (bohr.at(point).charrLinks[charr] == -1) // если ссылки нет
+		{
+			if (bohr.at(point).directLinks[charr] != -1) // проверяем есть ли ссылка в боре
+				bohr.at(point).charrLinks[charr] = bohr.at(point).directLinks[charr];
+			else // пробуем найти суффиксную ссылку
+			{
+				if (point == 0)
+					bohr.at(point).charrLinks[charr] = 0;
+				else
+					bohr.at(point).charrLinks[charr] = getLink(getSuffixLink(point), charr);
+			}
+		}
+		cout << "link to point " << bohr.at(point).charrLinks[charr] << " by char " << alphabet[charr] << endl;
+		return bohr.at(point).charrLinks[charr];
+	}
+
+	int getCompressedLink(int point) // // функция для получения конечной ссылки
+	{
+		if (bohr.at(point).compressedLink == -1) // если конечной ссылки нет
+		{
+			int curr = getSuffixLink(point); // получаем суффиксную ссылку
+			if (curr == 0)
+				bohr.at(point).compressedLink = 0;
+			else
+			{
+				if (bohr.at(curr).terminal)// если суффиксная ссылка указывает на конечную вершину
+					bohr.at(point).compressedLink = curr; // возвращаем конечную ссылку на конечную вершину
+				else // иначе пробуем запустить функцию для найденной вершины
+					bohr.at(point).compressedLink = getCompressedLink(curr);
+			}
+		}
+		if (bohr.at(point).compressedLink)
+			cout << "Compressed link to point " << bohr.at(point).compressedLink << "!\n";
+		else
+			cout << "Compressed link not found" << endl;
+		return bohr.at(point).compressedLink;
+	}
+
+	void find(int v, int i) // функция для проверки терминальности текущей вершины и запуска поиска конечных ссылок
+	{
+		int j = 0;
+		for (int u(v); u != 0; u = getCompressedLink(u), j++)
+		{
+			if (bohr.at(u).terminal) // если найдена конечная вершина
+			{
+				cout << "Terminal point was found!" << endl;
+				int delta = 0;
+				int curr = u;
+				while (bohr.at(curr).parentIndex != 0)
+				{
+					curr = bohr.at(curr).parentIndex;
+					delta++; // получаем размер паттерна, с помощью возврата до корня
+				}
+				cout << "\n-----\nPattern was found in index " << i - delta << ". Patter number - " << bohr.at(u).num << "\n-----\n\n";
+				pats.push_back({ delta, i - delta, bohr.at(u).num });
+			}
+		}
+
+	}
+
+	void AHO_COR() // запуск алгоритма Ахо-Корасик
+	{
+		cout << "\n-----\nSearching patterns in text \n\n";
+		int point = 0, charr = 0;
+		for (int i(0); i < text.length(); i++) // перебор всех символов текста
+		{
+			cout << "-----\nNew char: " << text.at(i) << endl << endl;
+			switch (text.at(i))
+			{
+			case 'A':
+				charr = 0;
+				break;
+			case 'C':
+				charr = 1;
+				break;
+			case 'G':
+				charr = 2;
+				break;
+			case 'T':
+				charr = 3;
+				break;
+			case 'N':
+				charr = 4;
+				break;
+			}
+			point = getLink(point, charr);
+			cout << "Current point: " << point << endl;
+			find(point, i + 1);
+		}
+
+		cout << "\n-----\nVariant 2 -- Indivualization" << endl;
+		bool flag = true;
+		for (int i = 0; i < pats.size(); i++)
+		{
+			int a, b;
+			for (int j = 0; j < pats.size(); j++)
+			{
+				if (pats.at(j).index > pats.at(i).index)
+				{
+					a = j;
+					b = i;
+				}
+				else
+				{
+					a = i;
+					b = j;
+				}
+				if (i != j && pats.at(a).index < pats.at(b).index + pats.at(b).size)
+				{
+					flag = false;
+					cout << "Adjacent patterns: pattern " << pats.at(b).num << " and " << pats.at(a).num;
+					cout << ", index " << pats.at(b).index << " and " << pats.at(a).index << endl;
+				}
+			}
+			pats.erase(pats.begin());
+
+		}
+		cout << "Points number: " << bohr.size() << endl;
+		if (flag) cout << "No adjusting patterns!\n";
+		printBohr(); // вывод автомата
+	}
+
+	void printBohr()
+	{
+		cout << "\n-----\nMachine built during the operation of the algorithm\n\n";
+		for (int i = 0; i < bohr.size(); i++)
+		{
+			cout << "Point " << i << " with paths: \n";
+			for (int j = 0; j < 5; j++)
+				if (bohr.at(i).charrLinks[j] != -1)
+					cout << "\tPoint " << bohr.at(i).charrLinks[j] << " with paht " << alphabet[j] << endl;
+		}
+	}
+};
+
+int main()
+{
+	Bohr bor;
+	bor.AHO_COR();
+	return 0;
+}
+/*ACGTNNNACGT 3 ACG CGT NNN*/
+/*ACGTNNN 3 ACG ACGT AC*/
---
+++ Bykov/lab5/Source/Joker/main.cpp
@@ -0,0 +1,264 @@
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Bohr
+{
+private:
+	string alphabet = "ACGTN";
+
+	struct Numbers
+	{
+		int index; // индекс в тексте
+		int patternNum; // индекс в строке паттерна
+	};
+
+	struct BohrPoint
+	{
+		int directLinks[5]; // переходы в боре
+		int patternNum[40]; // номер паттерна
+		int suffixLink;// суффиксная ссылка
+		int charrLink[5]; // переходы в автомате
+		int parentIndex; // индекс вершины родителя
+		int compressedLink; // переход по конечной ссылке
+		bool terminal; // является ли вершина конечной
+		char charr; // символ перехода от родителя
+	};
+
+	vector <BohrPoint> bohr; // бор
+	string text; // строка поиска
+	string patternStr; // строка c паттерном
+	vector <string> patterns; // вектор хранения подстрок - паттернов
+	vector <int> patternPos; // вектор хранения индексов паттернов
+	vector<Numbers> num; // вектор хранния найденных паттернов
+
+public:
+	Bohr() // создание бора
+	{
+		cout << "Creating bohr\n";
+		bohr.push_back(createPoint(-1, -1));
+		char joker;
+		cin >> text >> patternStr >> joker;
+		for (int i = 0; i < patternStr.size(); i++)
+		{
+			string pat;
+			if (patternStr[i] != joker)
+			{
+				patternPos.push_back(i + 1);
+				for (int j(i); patternStr[j] != joker && j != patternStr.length(); j++)
+				{
+					pat += patternStr[j];
+					i++;
+				}
+				cout << "\n-----\nNew pattern - " << pat << endl;
+				cout << "Add symbols of new pattern in prefix tree\n";
+				patterns.push_back(pat);
+				addString(pat); // добавляем паттерн в бор
+			}
+		}
+	}
+
+	BohrPoint createPoint(int p, char c) // функция для создания вершины в боре
+	{
+		BohrPoint v;
+		memset(v.directLinks, -1, sizeof(v.directLinks));
+		memset(v.charrLink, -1, sizeof(v.charrLink));
+		memset(v.patternNum, -1, sizeof(v.patternNum));
+		v.terminal = false;
+		v.suffixLink = -1;
+		v.parentIndex = p;
+		v.charr = c;
+		v.compressedLink = -1;
+		return v;
+	}
+
+	void addString(string& s) // функция для добавления паттерна
+	{
+		int index = 0;
+		for (int i = 0; i < s.size(); i++)
+		{
+			cout << "\nReceived character " << s[i] << endl;
+			char ch = alphabet.find(s[i]);
+			if (bohr[index].directLinks[ch] == -1) // если такого состояния в боре нет, то добавляем новое
+			{
+				cout << "Such way not found, adding new point with number " << bohr.size() << endl;
+				bohr.push_back(createPoint(index, ch));
+				bohr[index].directLinks[ch] = bohr.size() - 1;
+			}
+			index = bohr[index].directLinks[ch];
+			cout << "Switching to point - " << index << "\n";
+		}
+		bohr[index].terminal = true;
+		for (int i = 0; i < 40; i++)
+		{
+			if (bohr[index].patternNum[i] == -1)
+			{
+				bohr[index].patternNum[i] = patterns.size() - 1; // запоминаем номер паттерна
+				break;
+			}
+		}
+	}
+
+	int getSuffixLink(int point) // функция для получения суффиксной ссылки
+	{
+		if (bohr[point].suffixLink == -1) // если суффиксная ссылка не существует
+			if (point == 0 || bohr[point].parentIndex == 0) // если текущая вершина - корень или если родительская вершина - корень
+				bohr[point].suffixLink = 0; // возвращаем 0
+			else // иначе ищем ссылку на нижнем уровне
+				bohr[point].suffixLink = getLink(getSuffixLink(bohr[point].parentIndex), bohr[point].charr);
+		if (bohr.at(point).suffixLink)
+			cout << "Suffix link to point " << bohr.at(point).suffixLink << "!\n";
+		else
+			cout << "Suffix link not found\n";
+		return bohr.at(point).suffixLink;
+	}
+
+	int getLink(int point, char charr) // функция для получения обычной ссылки
+	{
+		if (bohr[point].charrLink[charr] == -1) // если ссылки нет
+			if (bohr[point].directLinks[charr] != -1) // проверяем есть ли ссылка в боре
+				bohr[point].charrLink[charr] = bohr[point].directLinks[charr];
+			else // пробуем найти суффиксную ссылку
+			{
+				if (point == 0)
+					bohr[point].charrLink[charr] = 0;
+				else
+					bohr[point].charrLink[charr] = getLink(getSuffixLink(point), charr);
+			}
+
+		cout << "link to point " << bohr.at(point).charrLink[charr] << " by char " << alphabet[charr] << endl;
+		return bohr.at(point).charrLink[charr];
+	}
+
+	int getCompressedLink(int point) // функция для получения конечной ссылки
+	{
+		if (bohr[point].compressedLink == -1) // если конечной ссылки нет
+		{
+			int curr = getSuffixLink(point);
+			if (curr == 0)
+				bohr[point].compressedLink = 0;
+			else
+			{
+				if (bohr[curr].terminal) // если суффиксная ссылка указывает на конечную вершину
+					bohr[point].compressedLink = curr; // возвращаем конечную ссылку на конечную вершину
+				else // иначе пробуем запустить функцию для найденной вершины
+					bohr[point].compressedLink = getCompressedLink(curr);
+			}
+		}
+		if (bohr.at(point).compressedLink)
+			cout << "Compressed link to point " << bohr.at(point).compressedLink << "!\n";
+		else
+			cout << "Compressed link not found" << endl;
+		return bohr.at(point).compressedLink;
+	}
+
+	void find(int v, int i) // функция для проверки терминальности текущей вершины и запуска поиска конечных ссылок
+	{
+		struct Numbers s;
+		for (int u(v); u != 0; u = getCompressedLink(u))
+		{
+			if (bohr[u].terminal) // если найдена конечная вершина
+			{
+				cout << "Terminal point was found!" << endl;
+				for (int j(0); j < 40; j++)
+				{
+					if (bohr[u].patternNum[j] != -1)
+					{
+						s.index = i - patterns[bohr[u].patternNum[j]].size(); // запоминаем индекс в строке
+						s.patternNum = bohr[u].patternNum[j]; // запоминаем номер паттерна
+						num.push_back(s);
+						cout << "\n-----\nPattern was found in index " << s.index << ". Pattern number - " << s.patternNum << "\n-----\n\n";
+					}
+					else
+						break;
+				}
+			}
+		}
+	}
+
+	void AHO() // запуск алгоритма Ахо-Корасик
+	{
+		cout << "\n-----\nSearching patterns in text \n\n";
+		int u = 0;
+		for (int i(0); i < text.size(); i++) // перебор всех символов текста
+		{
+			cout << "-----\nNew char: " << text.at(i) << endl << endl;
+			u = getLink(u, alphabet.find(text[i]));
+			find(u, i + 1);
+		}
+	}
+
+	void print() // вывод результатов
+	{
+		cout << "\n\n-----\nAnalysing found patterns\n";
+		vector <int> c(text.size(), 0);
+		for (int i(0); i < num.size(); i++)
+		{
+			cout << "Current pattern - " << patterns[num[i].patternNum] << endl;
+			if (num[i].index < patternPos[num[i].patternNum] - 1)
+				continue;
+			c[num[i].index - patternPos[num[i].patternNum] + 1]++;
+			cout << "On index " << num[i].index - patternPos[num[i].patternNum] + 2 << " - coincidence " << c[num[i].index - patternPos[num[i].patternNum] + 1] << " out of " << patterns.size() << endl;
+			if (c[num[i].index - patternPos[num[i].patternNum] + 1] == patterns.size() &&
+				num[i].index - patternPos[num[i].patternNum] + 1 <= text.size() - patternStr.size())
+				cout << "Whole patter was found on " << num[i].index - patternPos[num[i].patternNum] + 2 << endl;
+		}
+
+		cout << "\n-----\nVariant 2 -- Indivualization" << endl;
+		bool flag = true;
+		for (int i = 0; i < num.size(); i++)
+		{
+
+			int a, b;
+			for (int j = 0; j < num.size(); j++)
+			{
+				if (num.at(j).index > num.at(i).index)
+				{
+					a = j;
+					b = i;
+				}
+				else
+				{
+					a = i;
+					b = j;
+				}
+				if (i != j && num.at(a).index < num.at(b).index + patterns[num[b].patternNum].size())
+				{
+					flag = false;
+					cout << "Adjacent patterns: pattern " << num.at(b).patternNum << " and " << num.at(a).patternNum;
+					cout << ", index " << num.at(b).index << " and " << num.at(a).index << endl;
+				}
+			}
+			num.erase(num.begin());
+
+		}
+		cout << "Points number: " << bohr.size() << endl;
+		if (flag) cout << "No adjusting patterns!\n";
+		printBohr(); // вывод автомата
+
+	}
+
+	void printBohr()
+	{
+		cout << "\n-----\nMachine built during the operation of the algorithm\n\n";
+		for (int i = 0; i < bohr.size(); i++)
+		{
+			cout << "Point " << i << " with paths: \n";
+			for (int j = 0; j < 5; j++)
+				if (bohr.at(i).charrLink[j] != -1)
+					cout << "\tPoint " << bohr.at(i).charrLink[j] << " with paht " << alphabet[j] << endl;
+		}
+	}
+};
+
+int main()
+{
+
+	Bohr obj;
+	obj.AHO();
+	obj.print();
+	return 0;
+}/*ACGNCGTACGT ACG%CGT %*/
